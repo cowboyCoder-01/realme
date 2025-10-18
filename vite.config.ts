dce5b615f36d05e161d2759675c22b252005a210
cowboyCoder-01 <jseeley3@live.esu.edu>
2025-10-18 16:30:16 -0400

    try again

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build',
-    sourcemap: true
-  },
-  base: './',
-  server: {
-    port: 3000
-  }
 })
